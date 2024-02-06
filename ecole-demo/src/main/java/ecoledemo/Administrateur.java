35becba2d29ddbcce18f796044d6aac24b6cb4cd
josiasdekpe <deffonj@gmail.com>
2024-02-06 04:59:01 +0100

    Création classe Administrateur

---
+++ ecole-demo/src/main/java/ecoledemo/Administrateur.java
@@ -0,0 +1,13 @@
+package ecoledemo;
+
+public class Administrateur {
+    private String identifiant;
+    private String motDePasse;
+
+    public Administrateur(String identifiant, String motDePasse) {
+        this.identifiant = identifiant;
+        this.motDePasse = motDePasse;
+    }
+
+    // Ajout de getters et des setters si nécessaire.
+}
---
+++ ecole-demo/src/main/java/ecoledemo/Main.java
@@ -0,0 +1,12 @@
+package ecoledemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // Créez un administrateur par défaut
+        Administrateur admin = new Administrateur("admin", "admin");
+
+	}
+
+}
